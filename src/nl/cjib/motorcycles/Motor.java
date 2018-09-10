a529c0754a1754a2dec091a5fa23e3b19489ef6c
Anneke <walannek@hotmail.com>
2018-09-10 16:33:21 +0200

    Class Motor aangepast

--- src/nl/cjib/motorcycles/Motor.java
+++ src/nl/cjib/motorcycles/Motor.java
@@ -1,4 +1,41 @@
 package nl.cjib.motorcycles;
 
 public class Motor {
+
+    private String colour;
+    private int pk;
+    private int totalKM;
+    private String fuel;
+
+    public String getColour() {
+        return this.colour;
+    }
+
+    public void setColour (String colour) {
+        this.colour = colour;
+    }
+
+    public int getPk () {
+        return this.pk;
+    }
+
+    public void setPk (int pk) {
+        this.pk = pk;
+    }
+
+    public int getTotalKM() {
+        return totalKM;
+    }
+
+    public void setTotalKM(int totalKM) {
+        this.totalKM = totalKM;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
 }

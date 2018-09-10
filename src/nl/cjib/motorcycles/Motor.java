4079e5c5b33f3580dd002fb5dc5c8b577272f97c
Pascal van der Broek <pascalvanderbroek@gmail.com>
2018-09-10 15:44:13 +0200

    Versie 1.0

--- src/nl/cjib/motorcycles/Motor.java
+++ src/nl/cjib/motorcycles/Motor.java
@@ -1,4 +1,26 @@
 package nl.cjib.motorcycles;
 
+import java.sql.SQLOutput;
+
 public class Motor {
+
+    public static void main(String[] args) {
+
+        Belasting zaken = new Belasting();
+        zaken.setName("Wegenbeslasting");
+        zaken.setBedrag(70);
+        zaken.setPeriode("Maandelijks");
+
+        System.out.println(zaken.getName());
+        System.out.println(zaken.getBedrag());
+        System.out.println(zaken.getPeriode());
+
+
+
+    }
+
+
+
+
+
 }
